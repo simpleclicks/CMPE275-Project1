03ec34093884e72bd9a218886e033cbd3ab35113
Saurabh Jakate <saurabh.jakate@sjsu.edu>
2013-10-31 14:11:30 -0700

    Added Document Class

---
+++ src/poke/server/storage/jdbc/Document.java
@@ -0,0 +1,76 @@
+package poke.server.storage.jdbc;
+
+public class Document {
+	
+
+	private String documentName;
+	
+	private String namespaceName;
+	
+	private String owner;
+	
+	private String replicatedNode;
+	
+	private String previousReplicatedNode;
+	
+	private int replicationCount;
+	  
+	private boolean isReplicated;
+
+	public String getDocumentName() {
+		return documentName;
+	}
+
+	public void setDocumentName(String documentName) {
+		this.documentName = documentName;
+	}
+
+	public String getNamespaceName() {
+		return namespaceName;
+	}
+
+	public void setNamespaceName(String namespaceName) {
+		this.namespaceName = namespaceName;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getReplicatedNode() {
+		return replicatedNode;
+	}
+
+	public void setReplicatedNode(String replicatedNode) {
+		this.replicatedNode = replicatedNode;
+	}
+
+	public String getPreviousReplicatedNode() {
+		return previousReplicatedNode;
+	}
+
+	public void setPreviousReplicatedNode(String previousReplicatedNode) {
+		this.previousReplicatedNode = previousReplicatedNode;
+	}
+
+	public int getReplicationCount() {
+		return replicationCount;
+	}
+
+	public void setReplicationCount(int replicationCount) {
+		this.replicationCount = replicationCount;
+	}
+
+	public boolean isReplicated() {
+		return isReplicated;
+	}
+
+	public void setReplicated(boolean isReplicated) {
+		this.isReplicated = isReplicated;
+	}
+
+}
