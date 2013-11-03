65bc01ffc2964a55b1dfbbc7398aed32c82da676
KaustubhDeshmukh <kaustubh.deshmukh@outlook.com>
2013-11-03 14:03:24 -0800

    Conf changes

--- src/poke/demo/Jab.java
+++ src/poke/demo/Jab.java
@@ -38,9 +38,9 @@ public class Jab {
 		ClientListener listener = new ClientPrintListener("jab demo");
 		cc.addListener(listener);
 
-		//cc.docAddReq("Diwali", "E://test3.txt");
+		cc.docAddReq("Diwali1", "E://test3.txt");
 		
-	cc.docRemove("Diwali", "test3.txt");
+	//cc.docRemove("Diwali", "test3.txt");
 	
 	}
 
